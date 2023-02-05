35da970969b5ff9a7e9661abcfed5e809ab73def
Samuel <samuel.stroot@gmail.com>
2023-02-06 00:32:21 +0100

    Uhr 5.2.23 23:52

--- src/Uhrenszene.java
+++ src/Uhrenszene.java
@@ -1,26 +1,27 @@
 import GLOOP.*;
 import java.util.Calendar;
 public class Uhrenszene {
-    private GLEntwicklerkamera kamera;
-    private GLLicht licht;
     private GLTastatur tastatur;
-    private Wand wand;
-    private Uhr uhr1, uhr2, uhr3;
+    private Uhr uhr1, uhr2, uhr3, uhr4, uhr5, uhr6;
 
     private GLTafel datum;
-    int anpassungStu, anpassungMin, anpassungSek, anpassungDatum, zeitJetzt;
+    int anpassungStu, anpassungMin, anpassungSek, anpassungDatum, anpassungStuDublin;
 
-
+    int anpassungStuSydney, anpassungStuSeoul, anpassungStuHonolulu, anpassungStuMoskau;
     public void Uhrenszene() {
-        kamera = new GLEntwicklerkamera();
-        licht = new GLLicht(50, 50, 50);
+        GLEntwicklerkamera kamera = new GLEntwicklerkamera();
+        GLLicht licht = new GLLicht(100, 250, 400);
         tastatur = new GLTastatur();
-        kamera.setzePosition(50, 50, 1000);
-        wand = new Wand();
+        kamera.setzePosition(0, 250, 2000);
+        Wand wand = new Wand();
         wand.Wand(0, 0, -10);
         uhr1 = new Uhr(0, 0);
-        uhr2 = new Uhr(0,450);
-        datum = new GLTafel(140,0,0,55,35);
+        uhr2 = new Uhr(0,500);
+        uhr3 = new Uhr(500,500);
+        uhr4 = new Uhr(500,0);
+        uhr5 = new Uhr(-500,500);
+        uhr6 = new Uhr(-500,0);
+        datum = new GLTafel(139,0,-3,55,40);
         Zeitanpassung();
 
     }
@@ -28,25 +29,37 @@ public class Uhrenszene {
 
             public void Zeitanpassung(){
                     Calendar jetzt = Calendar.getInstance();
+
                     //Calendar heute = Calendar.getInstance();
                     anpassungStu = (jetzt.get(Calendar.HOUR_OF_DAY));// passt die Stunde an
                     anpassungMin = (jetzt.get(Calendar.MINUTE));// passt die minute an
                     anpassungSek = (jetzt.get(Calendar.SECOND));// passt die sekunde an
                     anpassungDatum = (jetzt.get(Calendar.DATE));
 
-                    datum.setzeText(String.valueOf(anpassungDatum),25);
+                    datum.setzeText(String.valueOf(anpassungDatum),25); //datum
 
+                    //------NEU--------
+                    anpassungStuDublin = (jetzt.get(Calendar.HOUR_OF_DAY)-1);
+                    anpassungStuSydney = (jetzt.get(Calendar.HOUR_OF_DAY)+10);
+                    anpassungStuSeoul = (jetzt.get(Calendar.HOUR_OF_DAY)+8);            //-Stunden von Uhren
+                    anpassungStuHonolulu = (jetzt.get(Calendar.HOUR_OF_DAY)-11);
+                    anpassungStuMoskau = (jetzt.get(Calendar.HOUR_OF_DAY)+2);
+                    //------NEU--------
 
-                    if(anpassungStu>12) anpassungStu = anpassungStu - 12;           //
+                    if(anpassungStu>12) anpassungStu = anpassungStu - 12;      //-12 wegen 12 strichen auf ziffernblatt
 
-
+                    //------NEU--------
+                    if(anpassungStuDublin>12) anpassungStu = anpassungStu - 12;
+                    if(anpassungStu>12) anpassungStu = anpassungStu - 12;
+                    if(anpassungStu>12) anpassungStu = anpassungStu - 12;
+                    if(anpassungStu>12) anpassungStu = anpassungStu - 12;
+                    if(anpassungStu>12) anpassungStu = anpassungStu - 12;
 
                     System.out.println(anpassungStu);
-                    System.out.println(anpassungMin);
+                    System.out.println(anpassungMin);       //check ob zeiten kommen
                     System.out.println(anpassungSek);
 
-
-                                                    //HIER FEHLER
+                                                    //HIER war FEHLER
                     anpassungMin=anpassungMin*60+anpassungSek;
                     anpassungStu=anpassungStu*60*60+anpassungMin;
 
--- src/Wand.java
+++ src/Wand.java
@@ -2,7 +2,7 @@ import GLOOP.*;
 public class Wand {
 GLQuader wand;
 public void Wand(int pX,int pY, int pZ){
-    wand = new GLQuader(pX,pY,pZ,1000,500,10);
+    wand = new GLQuader(pX,pY,pZ,10000,5000,10);
     wand.setzeFarbe(0,1,0);
 
 }
